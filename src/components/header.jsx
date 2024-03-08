a7e456e829efc622337353e26884dd6be23a68c9
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-08 17:14:00 +0000

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,9 @@
 function resHeader(){
     return(
-        <nav className="navbar navbar-dark bg-dark">
-        <div className="collapse navbar-collapse" id="navbarText">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      </ul>
-      </div>
+        <nav className="navbar navbar-light bg-light">
+        <span className="navbar-text">
+          Navbar text with an inline element
+        </span>
       </nav>
     )
 }

0dc77ce121dcfaadd270a84dc31fbb15bc963c36
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-08 17:23:13 +0000

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 function resHeader(){
     return(
-        <nav className="navbar navbar-light bg-light">
+        <>
+        <nav className="navbar navbar-light bg-light" >
         <span className="navbar-text">
           Navbar text with an inline element
         </span>
       </nav>
+      </>
     )
 }
 

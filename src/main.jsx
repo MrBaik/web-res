af8a17013f9beb47a399aabbb1338d7d4a88a10e
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-09 14:02:32 +0000

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-//import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// //import './index.css'
 import ResHeader from './components/header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<ResHeader />
+ 
     <App />
   </React.StrictMode>,
 )
